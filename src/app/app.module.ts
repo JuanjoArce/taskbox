b61bcdc5c6906815e1abf6e3cde1723395b8b2c4
JuanjoArce <juanjosearceherrera@gmail.com>
2022-10-12 19:41:06 -0300

    datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,32 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+ import { TaskModule } from './components/task.module';
+ import { NgxsModule } from '@ngxs/store';
+ import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+ import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+
+ import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
-import { TaskListComponent } from './components/task-list.component';
+import { PureTaskListComponent } from './components/pure-task-list.component';
+import { CommonModule } from '@angular/common';
+import { TaskComponent } from './components/task.component';
 
 @NgModule({
-  declarations: [AppComponent, TaskListComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, PureTaskListComponent, TaskComponent],
+  imports: [
+    BrowserModule,
+   TaskModule,
+   CommonModule,
+    NgxsModule.forRoot([], {
+     developmentMode: !environment.production,
+   }),
+   NgxsReduxDevtoolsPluginModule.forRoot(),
+   NgxsLoggerPluginModule.forRoot({
+     disabled: environment.production,
+   }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
